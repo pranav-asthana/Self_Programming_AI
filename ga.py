c1a118dfd122d4f6453a2dbd4ad32308b1f4e94e
Pranav Asthana <asthana_pranav@yahoo.co.in>
2017-04-09 14:50:11 +0530

    ..

--- ga.py
+++ ga.py
@@ -3,24 +3,25 @@ import math
 import random
 
 class pop_member:
-    def __init__(self, length):
+    def __init__(self):
         self.seq = ['+', '-', '.', ',', '[', ']', '>', '<']
-        self.code = ''.join([random.choice(self.seq) for i in range(length)])
+        self.length = random.randint(2,100)
+        self.code = ''.join([random.choice(self.seq) for i in range(self.length)])
+        self.fitness = 0
 
 
 class Life:
     def __init__(self):
         self.pop_size = 1000;
         self.mutation_rate = 0.03;
+        self.num_gens = 10
         self.population = [None] * self.pop_size
-        self.fitness = [0] * self.pop_size
         self.mating_pool = list()
 
     def random_population(self):
         for i in range(self.pop_size):
             length = random.randint(2, 10)
-            self.population[i] = [pop_member(length).code]
-            self.population[i].append(length)
+            self.population[i] = pop_member()
 
     def evaluate_fitness(self, code_string, input = None):
         end = len(code_string)
@@ -57,11 +58,13 @@ class Life:
 
 
     def select(self): # Roulette wheel selection
-        max_fitness = max(self.fitness)
-        mean_fitness = sum(self.fitness) / max_fitness
+        fitnesses = [self.population[i].fitness for i in range(self.pop_size)]
+        # print(fitnesses)
+        max_fitness = max(fitnesses)
+        mean_fitness = sum(fitnesses) / max_fitness
 
         for i in range(self.pop_size):
-            fitness_normal = self.fitness[i]/max_fitness; # Normalize fitness between 0 and 1
+            fitness_normal = fitnesses[i] / max_fitness; # Normalize fitness between 0 and 1
             n = int(fitness_normal * 100);  # Arbitrary multiplier
             for j in range(n):
                 self.mating_pool.append(self.population[i]); # Based on fitness, probability of selection will be higher for higher fitness because that organism's genes will get added to the mating pool more number of times
@@ -78,10 +81,11 @@ class Life:
 
             self.population[i] = child_genome
 
-    def mutate(): # Mutate strlen and str with prob = mutation rate
-        pass
+    def mutate(self, gene): # Mutate strlen and str with prob = mutation rate
+        return gene
 
-    def crossover(): # Single/Multi point crossover
+    def crossover(self, mom, dad): # Single/Multi point crossover
+        return mom
         pass
 
 
@@ -93,17 +97,18 @@ def main():
     life.evaluate_fitness('')
 
     life.random_population()
-    for i in range(life.pop_size):
-        # print('code: ', life.population[i][0])
-        life.fitness[i] = life.evaluate_fitness(life.population[i][0])
-        # print('fitness: ', life.fitness[i])
-        # print('--------------------------------------------')
 
-    print('Max fitness: ', max(life.fitness))
-    print('Fittest individual: ', life.population[life.fitness.index(max(life.fitness))][0])
+    for gen in range(life.num_gens):
+        print('Gen: ', gen)
+        for i in range(life.pop_size):
+            # print('code: ', life.population[i][0])
+            life.population[i].fitness = life.evaluate_fitness(life.population[i].code)
+            # print('fitness: ', life.fitness[i])
+            # print('--------------------------------------------')
 
-    life.select()
-
+        life.select()
+        life.mate()
+        print('----------------------------------------')
 
 
 if __name__ == '__main__':
